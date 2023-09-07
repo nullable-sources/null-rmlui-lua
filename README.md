ee860a4c9efddc96d664d9f7e27214cf5264e6fd
suddenly <suddenlybydl0@gmail.com>
2023-09-07 14:15:33 +0500

    🥴

---
+++ README.md
@@ -0,0 +1,1 @@
+The [original](https://github.com/LoneBoco/RmlSolLua) RmlUI sol lua bindings, but adapted for use in projects.
---
+++ src/null-rmlui-lua.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <RmlUi/Core.h>
+#include <null-lua.h>
+
+namespace null::rml::modules::lua {
+    void initialize(sol::state_view& state_view);
+    void register_bindings(sol::state_view& state_view);
+}
